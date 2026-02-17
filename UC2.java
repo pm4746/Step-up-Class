74b3adc106dcd83f73f3991e11d7b52f85e74bac
pm4746 <pm4746@srmist.edu.in>
2026-02-17 08:33:35 +0530

    Create UC2.java

---
+++ UC2.java
@@ -0,0 +1,25 @@
+/**
+ * OOPSBannerApp UC2 – Banner Display Application
+ *
+ * This class demonstrates the creation of a visual banner displaying
+ * the word "OOPS" using ASCII art composed of asterisks (*) and spaces.
+ * The banner is printed using individual print statements for each line.
+ *
+ * @author Developer
+ * @version 2.0
+ */
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("  *****  " + "  *****  " + " ******  " + "  *****  ");
+        System.out.println(" **   ** " + " **   ** " + " **   ** " + " **   ** ");
+        System.out.println(" **   ** " + " **   ** " + " **   ** " + " **      ");
+        System.out.println(" **   ** " + " **   ** " + " ******  " + "  *****  ");
+        System.out.println(" **   ** " + " **   ** " + " **      " + "      ** ");
+        System.out.println(" **   ** " + " **   ** " + " **      " + " **   ** ");
+        System.out.println("  *****  " + "  *****  " + " **      " + "  *****  ");
+
+    }
+}
